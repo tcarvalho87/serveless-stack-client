295d2d239c6adeae64a40b126865b692eb48a24e
Thiago Carvalho <tcarvalho87@gmail.com>
2020-01-28 23:17:56 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-upload87"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://y3e5sdlo08.execute-api.us-west-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_FNrkXTcD1",
+      APP_CLIENT_ID: "2vrlbsunh438i2kqdkrb0o1tmb",
+      IDENTITY_POOL_ID: "us-west-2:beb3ac87-abce-4b80-9d7a-8202f6d21e57"
+    }
+  };
